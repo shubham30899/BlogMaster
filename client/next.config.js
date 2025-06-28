ac75103d272b8b33808f3029630e78979e5f870d
shubham30899 <sgmtr985@gmail.com>
2025-06-28 05:40:20 +0530

    made env files

--- client/next.config.js
+++ client/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
